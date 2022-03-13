1698b819661b804ef50f949adc600f19e1c34881
almoukdaad494 <almoukdaadwaar94@outlook.com>
2022-03-13 20:50:54 +0100

    aantal huis vestingingen API

--- js.js
+++ js.js
@@ -1,18 +1,3 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
- 
-app.use(cors())
- 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
- 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
-
 var cont=document.querySelector('.cont'); // container
 var divEl=maakEl('div','');
 var errors=maakEl('p',"");
